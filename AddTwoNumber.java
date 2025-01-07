8add9fccbef0f905f437be5697eaefd0bd30b263
naveenjha031 <naveenjha344@gmail.com>
2025-01-08 01:10:07 +0530

    SecondCode

---
+++ AddTwoNumber.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class AddTwoNumber{
+   public static void main(String[] args)
+{
+   Scanner sc = new Scanner(System.in);
+   System.out.println("Type the num 1 :");
+   int x = sc.nextInt();
+   System.out.println("Tell the num 2 :");
+   int y = sc.nextInt();
+    
+   System.out.println("The sum of two number are : " + (x+y));
+}
+}
